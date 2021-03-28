48066a784c8a3a5185653eda624c048df23a73b0
Noeliel <noelieldev@gmail.com>
2021-03-28 23:07:30 +0200

    Small fix in stop.sh

--- stop.sh
+++ stop.sh
@@ -3,7 +3,7 @@
 # Set up path to Moodle code
 export MOODLE_DOCKER_WWWROOT=/srv/moodle
 # Choose a db server (Currently supported: pgsql, mariadb, mysql, mssql, oracle)
-export MOODLE_DOCKER_DB=mysql
+export MOODLE_DOCKER_DB=pgsql
 
 # Shut down and destroy containers
 bin/moodle-docker-compose down
